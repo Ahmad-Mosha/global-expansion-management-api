db88d605a94ae51209ac12fd6c25d9e075f13110
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2025-08-21 12:23:14 +0300

    setup project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ClientsModule } from './clients/clients.module';
+import { VendorsModule } from './vendors/vendors.module';
+import { MatchesModule } from './matches/matches.module';
+import { AnalyticsModule } from './analytics/analytics.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ProjectsModule,
+    ClientsModule,
+    VendorsModule,
+    MatchesModule,
+    AnalyticsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
